ac3c517b0fd694c89067b8499df5521f258d9b07
291e <yko0321@naver.com>
2024-03-13 13:58:26 +0900

    vanilla5.0

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-const calculator = {
-  add: function (a, b) {
-    console.log(a, b);
-  },
-  mins: function (a, b) {
-    console.log(a, b);
-  },
-};
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+
+  /* if (username === "") {
+    alert("이름을 입력하세요.");
+  } else if (username.length > 15) {
+    alert("너무 긴 이름입니다.");
+  } */
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
